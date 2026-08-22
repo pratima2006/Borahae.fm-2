2d744c1e84ed7a9d7f01682fc44153f4afa4e353
pratima2006 <pratimatribhuvan2006@gmail.com>
2026-08-22 22:51:26 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ['"Instrument Serif"', "serif"],
+        sans: ["Inter", "sans-serif"],
+        mono: ['"Geist Mono"', "monospace"],
+      }
+    },
+  },
+  plugins: [],
+}
